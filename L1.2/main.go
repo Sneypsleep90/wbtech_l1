10718a90c8d7e25525c4492c718477254f7d34fa
magomedbersanov <magamed4924@mail.ru>
2025-09-06 20:03:24 +0300

    l1.2

---
+++ L1.2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var nums = []int{2, 4, 6, 8, 10}
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	for _, n := range nums {
+		wg.Add(1)
+		go func(num int) {
+			ch <- num * num
+			defer wg.Done()
+
+		}(n)
+
+	}
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for res := range ch {
+		fmt.Println(res)
+	}
+
+}

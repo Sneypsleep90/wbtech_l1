1d747d7c15ff86267eebb7a981c0e494ab0dbb99
magomedbersanov <magamed4924@mail.ru>
2025-09-08 22:44:57 +0300

    wb the best

---
+++ L1.15/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var justString string
+
+func createHugeString(size int) string {
+	return string(make([]byte, size))
+}
+func someFunc() {
+	v := createHugeString(1 << 10)
+	justString = string([]byte(v[:100]))
+}
+func main() {
+	someFunc()
+	fmt.Println(len(justString))
+}

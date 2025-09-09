ffc796aaa3392f70574ef885ebb7ce2582abcfee
magomedbersanov <magamed4924@mail.ru>
2025-09-09 18:29:59 +0300

    wb the best

---
+++ L1.19/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func reverse(str string) string {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	var str string
+	fmt.Scan(&str)
+	fmt.Println(reverse(str))
+}
